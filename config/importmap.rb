335af28b83f78adeecc801bad3e2aca6b6b54af7
sufyansohail <sufyansohail09@gmail.com>
2024-05-13 23:28:19 +0500

    scrape vue component added

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "vue", to: "https://ga.jspm.io/npm:vue@3.3.13/dist/vue.esm-browser.prod.js"
+pin "redaxios", to: "https://ga.jspm.io/npm:redaxios@0.5.1/dist/redaxios.module.js"
